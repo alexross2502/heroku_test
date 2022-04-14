9235d9056daeb072fb2acf7b21dd04e519ad9761
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-04-14 07:40:56 +0300

    test commit 31

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-const config = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 const app = express();
-const sequelize = require("./db");
-const models = require("./models/models");
-const router = require("./routes/index");
-const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+
 
 app.use(cors());
 app.use(express.json());
-app.use("https://intership-alex.herokuapp.com/api", router);
-//обработка ошибок, последний middleware
-app.use(errorHandler);
+
 
 app.get("https://intership-alex.herokuapp.com/", (req, res) => {
-  res.render("web/src/index");
+  res.render("https://www.youtube.com/");
 });
 
 const start = async () => {
