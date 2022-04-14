c381e9987920d146fa2a1659fab1ac122664e932
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-04-14 07:30:49 +0300

    test commit 30

--- routes/adminRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const adminController = require("../controllers/adminController");
-
-router.post("https://intership-alex.herokuapp.com/", adminController.check);
-
-module.exports = router;
